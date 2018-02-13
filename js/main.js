ae1f7ad2e6f9116a5944f9a8593f09aa793253f8
night18 <warrior.sp@gmail.com>
2018-02-13 17:23:11 -0500

    homepage

--- js/main.js
+++ js/main.js
@@ -9,6 +9,25 @@ function main() {
 (function () {
    'use strict';
 
+   $('a.page-scroll').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			console.log(target.offset().top);
+			if (target.length) {
+	            $('#main-content').animate({
+	              scrollTop: target.offset().top
+	            }, 700);
+	            return false;
+			}
+        }
+	});
+
+   $('body').scrollspy({ 
+        target: '.navbar-default',
+        offset: 80
+    })
+
 	$(document).ready(function () {
 		$('#sidebarCollapse').on('click', function () {
 			$('#left-sidebar').toggleClass('active');
