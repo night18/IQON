70687971c29707a48da61b512fe573063e1baaa2
night18 <warrior.sp@gmail.com>
2018-02-15 18:55:02 -0500

    add footer and change the scroller.

--- js/main.js
+++ js/main.js
@@ -1,32 +1,32 @@
-// Hello.
-//
-// This is The Scripts used for ___________ Theme
-//
-//
-
 function main() {
 
 (function () {
    'use strict';
+   let previousHeight = 0;
+   let newHeight = 0;
 
    $('a.page-scroll').click(function() {
         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
 			var target = $(this.hash);
 			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-			console.log(target.offset().top);
+						
+			newHeight = target.offset().top + previousHeight;
+			previousHeight = newHeight;
+
 			if (target.length) {
+
 	            $('#main-content').animate({
-	              scrollTop: target.offset().top
-	            }, 700);
+	              scrollTop:newHeight
+	            }, 900);
+
+	            if( $('#main-content').prop("scrollHeight") - $('#main-content').height() < previousHeight ){
+	            	previousHeight = $('#main-content').prop("scrollHeight") - $('#main-content').height();
+	            }
+
 	            return false;
 			}
         }
 	});
-
-   $('body').scrollspy({ 
-        target: '.navbar-default',
-        offset: 80
-    })
 
 	$(document).ready(function () {
 		$('#sidebarCollapse').on('click', function () {
