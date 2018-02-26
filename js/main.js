ebb7aa54498600484ffbc676e26e99e2fbf919b5
night18 <warrior.sp@gmail.com>
2018-02-26 16:04:59 -0500

    hide sidebar

--- js/main.js
+++ js/main.js
@@ -5,33 +5,35 @@ function main() {
    let previousHeight = 0;
    let newHeight = 0;
 
-   $('a.page-scroll').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-			var target = $(this.hash);
-			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+ //   $('a.page-scroll').click(function() {
+ //        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+	// 		var target = $(this.hash);
+	// 		target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
 						
-			newHeight = target.offset().top + previousHeight;
-			previousHeight = newHeight;
+	// 		newHeight = target.offset().top + previousHeight;
+	// 		previousHeight = newHeight;
 
-			if (target.length) {
+	// 		if (target.length) {
 
-	            $('#main-content').animate({
-	              scrollTop:newHeight
-	            }, 900);
+	//             $('#main-content').animate({
+	//               scrollTop:newHeight
+	//             }, 900);
 
-	            if( $('#main-content').prop("scrollHeight") - $('#main-content').height() < previousHeight ){
-	            	previousHeight = $('#main-content').prop("scrollHeight") - $('#main-content').height();
-	            }
+	//             if( $('#main-content').prop("scrollHeight") - $('#main-content').height() < previousHeight ){
+	//             	previousHeight = $('#main-content').prop("scrollHeight") - $('#main-content').height();
+	//             }
 
-	            return false;
-			}
-        }
-	});
+	//             return false;
+	// 		}
+ //        }
+	// });
 
 	$(document).ready(function () {
-		// $('#sidebarCollapse').on('click', function () {
-		// 	$('#left-sidebar').toggleClass('active');
-		// });
+		$('#menu-bar').on('click', function () {
+			$('#left-sidebar').toggleClass('active');
+      $('#main-content').toggleClass('active');
+      $('#menu-bar').toggleClass('active');
+		});
 
     $('#main-content').fullpage({
       loopHorizontal: false,
