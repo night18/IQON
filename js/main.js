7a03f2096052f7638dcb4f5b388252e5a68af3e1
kobejie <1125277033@qq.com>
2018-03-19 17:45:35 -0400

    piechart

--- js/main.js
+++ js/main.js
@@ -291,6 +291,100 @@ function main() {
       
     });
     
+	
+var chart;
+var legend;
+var selected;
+
+var types = [{
+  type: "Fossil Energy",
+  percent: 70,
+  color: "#ff9e01",
+  subs: [{
+    type: "Oil",
+    percent: 15
+  }, {
+    type: "Coal",
+    percent: 35
+  }, {
+    type: "Nuclear",
+    percent: 20
+  }]
+}, {
+  type: "Green Energy",
+  percent: 30,
+  color: "#b0de09",
+  subs: [{
+    type: "Hydro",
+    percent: 15
+  }, {
+    type: "Wind",
+    percent: 10
+  }, {
+    type: "Other",
+    percent: 5
+  }]
+}];
+function generateChartData() {
+  var chartData = [];
+  for (var i = 0; i < types.length; i++) {
+    if (i == selected) {
+      for (var x = 0; x < types[i].subs.length; x++) {
+        chartData.push({
+          type: types[i].subs[x].type,
+          percent: types[i].subs[x].percent,
+          color: types[i].color,
+          pulled: true
+        });
+      }
+    } else {
+      chartData.push({
+        type: types[i].type,
+        percent: types[i].percent,
+        color: types[i].color,
+        id: i
+      });
+    }
+  }
+  return chartData;
+}
+
+	AmCharts.makeChart("chartdiv", {
+	"hideCredits":true,
+	"type": "pie",
+	"theme": "light",
+
+	"dataProvider": generateChartData(),
+	"labelText": "[[title]]: [[value]]",
+	"balloonText": "[[title]]: [[value]]",
+	"titleField": "type",
+	"valueField": "percent",
+	"outlineColor": "#FFFFFF",
+	"outlineAlpha": 0.4,
+	"outlineThickness": 2,
+	"colorField": "color",
+	"pulledField": "pulled",
+	"depth3D": 15,
+	"angle": 30,
+	"hoverAlpha": 0.8,
+	"fontFamily": "Cinzel Decorative",
+	"fontSize": 15,
+	"listeners": [{
+	"event": "clickSlice",
+	"method": function(event) {
+	  var chart = event.chart;
+	  if (event.dataItem.dataContext.id != undefined) {
+		selected = event.dataItem.dataContext.id;
+	  } else {
+		selected = undefined;
+	  }
+	  chart.dataProvider = generateChartData();
+	  chart.validateData();
+	}
+	}],
+	});
+	
+	
 	});
 
    
